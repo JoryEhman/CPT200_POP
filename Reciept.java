1a92dcd6020857c87bb6ef8d111d9d93e23c462f
JoryEhman <86196655+JoryEhman@users.noreply.github.com>
2022-03-18 16:14:33 -0500

    Order

--- Reciept.java
+++ Reciept.java
@@ -1,30 +1,69 @@
 package application;
 
-public class Reciept {
-	//Data fields
-	private String name, size, topping;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javafx.scene.control.CheckBox;
+
+public class Order {
+
+	public static String printToTotal(CheckBox sizeSmall, CheckBox sizeMed, CheckBox sizeLarge, CheckBox topping1, CheckBox topping2, CheckBox topping3, CheckBox topping4,
+			CheckBox side1, CheckBox side2, CheckBox side3, CheckBox side4,CheckBox drink1, CheckBox drink2, CheckBox drink3, CheckBox drink4) {
+			
+		
+			double totalPrice = 0.0;
+			
+			if (sizeSmall.isSelected()) {
+				totalPrice += 6.99;
+			}
+			if (sizeMed.isSelected()) {
+				totalPrice += 8.99;
+			}
+			if (sizeLarge.isSelected()) {
+				totalPrice += 10.99;
+			}
+			if (topping1.isSelected()) {
+				totalPrice += 1.50;
+			}
+			if (topping2.isSelected()) {
+				totalPrice += 1.50;
+			}
+			if (topping3.isSelected()) {
+				totalPrice += 1.50;
+			}
+			if (topping4.isSelected()) {
+				totalPrice += 1.50;
+			}
+			
+			if (side2.isSelected()) {
+				totalPrice += 8.00;
+			}
+			if (side3.isSelected()) {
+				totalPrice += 5.50;
+			}
+			if (side1.isSelected()) {
+				totalPrice += 5.50;
+			}
+			if (side4.isSelected()) {
+				totalPrice += 6.50;
+			}		
+			if (drink2.isSelected()) {
+				totalPrice += 2.00;
+			}
+			if (drink1.isSelected()) {
+				totalPrice += 2.00;
+			}
+			if (drink3.isSelected()) {
+				totalPrice += 2.00;
+			}
+			if (drink4.isSelected()) {
+				totalPrice += 2.00;
+			}
+			
+			String printedPrice = String.format("$%.2f", totalPrice);
+			
+			return printedPrice.formatted(printedPrice);
+			
+		}
 	
-	//Constructor
-	public Reciept(String name, String size, String topping) {
-		this.name = name;
-		this.size = size;
-		this.topping = topping;
-	}
-	
-	public Reciept(String name) {
-		this.name = name;
-	}
-	
-	//Getters
-	public String getName() { return name; }
-	public String getSize() { return size; }
-	public String getToppings() { return topping;}
-	
-	//Setters
-	public void setName(String name) {this.name = name; }
-	public void setSize(String size) {this.size = size; }
-	public void setTopping(String topping) {this.topping = topping; }
-	
-	//Override Methods
-	public String toString() { return (name + " | " + size + " | " + topping); }
 }
